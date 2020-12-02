a30c90371bf5b7268a4c936e5df4d2f7e251e492
Bill Mårtensson <bill@magictechnology.se>
2020-12-02 20:44:03 +0100

    Initial Commit

---
+++ MYTest/MYTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYTestApp.swift
+//  MYTest
+//
+//  Created by Bill Martensson on 2020-12-02.
+//
+
+import SwiftUI
+
+@main
+struct MYTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
